98c5e37bc06eb3733edda9c1a75c2d5f627b268f
Adam Myers <myers.adam.k@gmail.com>
2018-07-16 17:03:02 -0400

    Some concept code for codebook written

---
+++ src/main/java/com/exigentech/mtgapiclient/cards/client/CardBook.java
@@ -0,0 +1,44 @@
+package com.exigentech.mtgapiclient.cards.client;
+
+import static java.util.Arrays.stream;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class CardBook {
+
+  private final int pageCount;
+  private final URI baseURI;
+
+  public static CardBook create(CardPage page, URI baseURI) {
+    final var lastPageParam = stream(page.last().getQuery().split("&"))
+        .collect(
+            Collectors.toMap(s ->
+                    s.substring(0, s.indexOf("=")),
+                s -> s.substring(s.indexOf("="))
+            )
+        ).get("page");
+
+    if (lastPageParam == null) {
+      // hell
+    }
+
+    return new CardBook(Integer.valueOf(lastPageParam), baseURI);
+  }
+
+  public List<URI> range(int first, int last) {
+    return IntStream.range(0, pageCount).mapToObj(this::constructURI).collect(Collectors.toList());
+  }
+
+  private URI constructURI(final int pageNumber) {
+    return UriComponentsBuilder.fromUri(baseURI).queryParam("page", pageNumber).build().toUri();
+  }
+
+  public CardBook(int pageCount, URI baseURI) {
+    this.pageCount = pageCount;
+    this.baseURI = baseURI;
+  }
+}
