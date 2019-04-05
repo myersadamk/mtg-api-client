ae7f834005b8c541804faf6a0f9629599526b96c
Adam Myers <myers.adam.k@gmail.com>
2019-04-05 14:26:37 -0400

    Addressed some TODOs - removed some classes, now using custom exception rather than runtime, and moved page header stuff into a utility class

--- src/main/java/com/exigentech/mtgapiclient/cards/client/mtgio/MagicTheGatheringCardsClient.java
+++ src/main/java/com/exigentech/mtgapiclient/cards/client/mtgio/MagicTheGatheringCardsClient.java
@@ -1,20 +1,15 @@
 package com.exigentech.mtgapiclient.cards.client.mtgio;
 
-import static java.util.regex.Pattern.compile;
-import static org.springframework.http.HttpHeaders.LINK;
+import static com.exigentech.mtgapiclient.cards.client.mtgio.PageHeaderIntrospector.populateLinks;
 
 import com.exigentech.mtgapiclient.cards.client.CardsClient;
+import com.exigentech.mtgapiclient.cards.client.CardsClientException;
 import com.exigentech.mtgapiclient.cards.client.mtgio.model.Cards;
 import com.exigentech.mtgapiclient.cards.client.util.BodyParser;
 import com.exigentech.mtgapiclient.cards.model.CardPage;
 import com.exigentech.mtgapiclient.cards.model.ImmutableCardPage;
-import com.exigentech.mtgapiclient.cards.model.ImmutableCardPage.Builder;
 import java.net.URI;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -22,8 +17,7 @@ import reactor.core.publisher.Mono;
 @Component
 public final class MagicTheGatheringCardsClient implements CardsClient {
 
-  private final static URI BASE_URI = URI
-      .create("https://api.magicthegathering.io/v1/cards?page=1");
+  private final static URI BASE_URI = URI.create("https://api.magicthegathering.io/v1/cards?page=1");
 
   private final BodyParser parser;
   private final WebClient client;
@@ -53,29 +47,9 @@ public final class MagicTheGatheringCardsClient implements CardsClient {
 
     return publisher
         // TODO: create a specific exception class
-        .doOnError(RuntimeException::new)
+        .doOnError(error -> new CardsClientException("Call failed to magicthegatheriong.io", error))
         .doOnNext(spec -> populateLinks(uri, spec.headers().asHttpHeaders(), builder))
         .flatMap(spec -> spec.bodyToMono(String.class))
         .map(body -> builder.cards(parser.parse(Cards.class, body).cards()).build());
-  }
-
-  // TODO: throw into another class
-  private static final String LINK_RELATIONSHIP_PATTERN = "<([\\w:/\\.\\d\\?=]+)>; rel=\"%s\"";
-  private static final Pattern NEXT = compile(String.format(LINK_RELATIONSHIP_PATTERN, "next"));
-  private static final Pattern LAST = compile(String.format(LINK_RELATIONSHIP_PATTERN, "last"));
-
-  private void populateLinks(final URI self, final HttpHeaders headers, final Builder builder) {
-    builder.next(getLink(headers, NEXT));
-    builder.last(getLink(headers, LAST).orElse(self));
-  }
-
-  private Optional<URI> getLink(final HttpHeaders headers, final Pattern pattern) {
-    return headers.get(LINK).stream().map(content -> {
-      final Matcher matcher = pattern.matcher(content);
-      if (matcher.find() && matcher.groupCount() >= 1) {
-        return matcher.group(1);
-      }
-      return null;
-    }).filter(group -> group != null).findFirst().map(uri -> URI.create(uri));
   }
 }
---
+++ src/main/java/com/exigentech/mtgapiclient/cards/client/mtgio/PageHeaderIntrospector.java
@@ -0,0 +1,37 @@
+package com.exigentech.mtgapiclient.cards.client.mtgio;
+
+import static java.util.regex.Pattern.compile;
+import static org.springframework.http.HttpHeaders.LINK;
+
+import com.exigentech.mtgapiclient.cards.model.ImmutableCardPage.Builder;
+import java.net.URI;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.springframework.http.HttpHeaders;
+
+final class PageHeaderIntrospector {
+
+  // TODO: throw into another class
+  private static final String LINK_RELATIONSHIP_PATTERN = "<([\\w:/\\.\\d\\?=]+)>; rel=\"%s\"";
+  private static final Pattern NEXT = compile(String.format(LINK_RELATIONSHIP_PATTERN, "next"));
+  private static final Pattern LAST = compile(String.format(LINK_RELATIONSHIP_PATTERN, "last"));
+
+  static void populateLinks(final URI self, final HttpHeaders headers, final Builder builder) {
+    builder.next(getLink(headers, NEXT));
+    builder.last(getLink(headers, LAST).orElse(self));
+  }
+
+  private static Optional<URI> getLink(final HttpHeaders headers, final Pattern pattern) {
+    return headers.get(LINK).stream().map(content -> {
+      final Matcher matcher = pattern.matcher(content);
+      if (matcher.find() && matcher.groupCount() >= 1) {
+        return matcher.group(1);
+      }
+      return null;
+    }).filter(group -> group != null).findFirst().map(uri -> URI.create(uri));
+  }
+
+  private PageHeaderIntrospector() {
+  }
+}
